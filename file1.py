2664b74b0d35a489372ea3aef9d27afac07cfb71
saimahesh473 <162981935+saimahesh473@users.noreply.github.com>
2024-03-11 16:02:33 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,60 @@
+#print("hello world")
+#print("hello 1")
+
+
+
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+
+'''
+----> Commenting
+1. Commenting is used for making documentation in a python file
+2. When the line is commented, that line will not be considered as a part of the code.
+3. When the program is executed, commented line will not be in the part of execution.
+> NOTE:
+Single line commenting can be done using # at the begining of the line
+Eg:
+#print("hello 1")
+I Multiple line commenting can be done using code . The lines between the quotes will be commented.
+Eg:
+   
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+        
+print("hello world")
+
+print("hello world")
+
+'''
+import this
+#import is a keywordwhich is used to communicated between the python file
+
+import antigravity
+
+
+
+# ---> print(object, sep, end, file, flush)
+#print has 5 parameters
+
+ #object ->the value to be printed is object
+#sep--> seperator
+# Eg
+#a = 8
+#b = 9
+#c = 89
+# print(a, b, c, sep='$')
+
+# end --> which is used to add the string at the end of the value,
+# # It will not keep the command to stay in same line
+# Eg:
+# print(str(a), b, end='%%')
+# print(str(c))
+
